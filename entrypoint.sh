7a20964cc794d28efcd4ff78d788944ebd370ea1
Angie Brandt <angiebrandt@gmail.com>
2020-04-07 14:07:44 -0700

    remove chmod

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,6 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Allow registry.npmjs.org to be overridden with an environment variable
   printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
 
-  chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
 # initialize git
