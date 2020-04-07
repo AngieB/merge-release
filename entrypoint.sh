cbfa5fd4d4e02db7e020ca45e0c9573b85aecef9
Angie Brandt <angiebrandt@gmail.com>
2020-04-07 14:27:03 -0700

    mkdir first

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p $HOME/.npmrc ; 
+mkdir -p ${NPM_CONFIG_USERCONFIG} ; 
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
@@ -15,7 +15,6 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
     NPM_REGISTRY_SCHEME="http"
   fi
   
-
   # Allow registry.npmjs.org to be overridden with an environment variable
   printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
 
