1dda90c39294bf05829b97e378bad0b74a5228ff
Angie Brandt <angiebrandt@gmail.com>
2020-04-07 14:57:36 -0700

    fix vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,9 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
   NPM_STRICT_SSL="${NPM_STRICT_SSL-true}"
   NPM_REGISTRY_SCHEME="https"
-  if ! $NPM_STRICT_SSL
-  then
-    NPM_REGISTRY_SCHEME="http"
-  fi
   
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "${NPM_REGISTRY_SCHEME}://$NPM_REGISTRY_URL" "${NPM_STRICT_SSL}" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s\\nregistry=%s\\nstrict-ssl=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_SCHEME://$NPM_REGISTRY_URL" "$NPM_STRICT_SSL" > "$NPM_CONFIG_USERCONFIG"
 
 fi
 
